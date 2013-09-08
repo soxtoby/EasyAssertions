2ea3a46a4cb6192594bdaf5671a8717eeb4f6d40
Simon Oxtoby <soxtoby@zaptechnology.com>
2013-09-08 12:21:06 +1000

    Adding ShouldMatch for comparing trees that takes a custom equality function.

--- EasyAssertions/Assertions/TreeAssertions.cs
+++ EasyAssertions/Assertions/TreeAssertions.cs
@@ -17,5 +17,18 @@ namespace EasyAssertions
                         throw EasyAssertion.Failure(FailureMessageFormatter.Current.TreesDoNotMatch(expectedRootNodes, actualRootNodes, getChildren, Compare.ObjectsAreEqual, message));
                 });
         }
+
+        /// <summary>
+        /// Asserts that two trees have the same structure and values.
+        /// Values are compared using the default equality comparer.
+        /// </summary>
+        public static Actual<IEnumerable<TActual>> ShouldMatch<TActual, TExpected>(this IEnumerable<TActual> actualRootNodes, IEnumerable<TestNode<TExpected>> expectedRootNodes, Func<TActual, IEnumerable<TActual>> getChildren, Func<TActual, TExpected, bool> predicate, string message = null) where TExpected : TActual
+        {
+            return actualRootNodes.RegisterAssert(() =>
+                {
+                    if (!Compare.TreesMatch(actualRootNodes, expectedRootNodes, getChildren, predicate))
+                        throw EasyAssertion.Failure(FailureMessageFormatter.Current.TreesDoNotMatch(expectedRootNodes, actualRootNodes, getChildren, (a, e) => predicate((TActual)a, (TExpected)e), message));
+                });
+        }
     }
 }
--- UnitTests/Assertions/TreeAssertionTests.cs
+++ UnitTests/Assertions/TreeAssertionTests.cs
@@ -1,5 +1,6 @@
 ﻿using NSubstitute;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -31,7 +32,34 @@ namespace EasyAssertions.UnitTests
             Assert.AreEqual("bar", result.Message);
         }
 
-        private IEnumerable<T> NoChildren<T>(T node)
+        [Test]
+        public void ShouldMatchTree_CustomEquality_TreeMatches_ReturnsActual()
+        {
+            object actualItem = new object();
+            IEnumerable<object> actual = new[] { actualItem };
+
+            Actual<IEnumerable<object>> result = actual.ShouldMatch(new[] { new object().Node() }, NoChildren, (a, e) => a == actualItem);
+
+            Assert.AreSame(actual, result.And);
+        }
+
+        [Test]
+        public void ShouldMatchTree_CustomEquality_TreesAreDifferent_FailsWithTreesDoNotMatchMessage()
+        {
+            int[] actual = { 1 };
+            TestNode<int>[] expected = { 1 };
+            Func<int, int, bool> equality = Substitute.For<Func<int, int, bool>>();
+            Func<object, object, bool> formatterEquality = null;
+            MockFormatter.TreesDoNotMatch(expected, actual, NoChildren, Arg.Do<Func<object, object, bool>>(func => formatterEquality = func), "foo").Returns("bar");
+
+            EasyAssertionException result = Assert.Throws<EasyAssertionException>(() => actual.ShouldMatch(expected, NoChildren, equality, "foo"));
+
+            Assert.AreEqual("bar", result.Message);
+            formatterEquality(2, 3);
+            equality.Received()(2, 3);
+        }
+
+        private static IEnumerable<T> NoChildren<T>(T node)
         {
             return Enumerable.Empty<T>();
         }

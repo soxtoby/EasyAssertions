981787b5b9f337ea471c4c42753221a9c4e38d67
soxtoby <simonoxtoby@gmail.com>
2017-05-06 13:15:21 +1000

    Adding Enum assertions for equality and checking flags

---
+++ EasyAssertions/Assertions/EnumAssertions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EasyAssertions
+{
+    /// <summary>
+    /// Assertions for Enums.
+    /// </summary>
+    public static class EnumAssertions
+    {
+        /// <summary>
+        /// Asserts that two value types are equal, using the default equality comparer.
+        /// </summary>
+        public static Actual<T> ShouldBeValue<T>(this T actual, T expected, string message = null) where T : struct
+        {
+            return actual.RegisterAssertion(c =>
+                {
+                    if (!c.Test.ObjectsAreEqual(actual, expected))
+                        throw c.StandardError.NotEqual(expected, actual, message);
+                });
+        }
+
+        public static Actual<T> ShouldHaveFlag<T>(this T actual, T expectedFlag, string message = null) where T : struct
+        {
+            return actual.RegisterAssertion(c =>
+                {
+                    if (!typeof(Enum).IsAssignableFrom(typeof(T)))
+                        throw c.StandardError.NotEqual(typeof(Enum), actual.GetType(), message);
+
+                    if (typeof(T).GetCustomAttributes(typeof(FlagsAttribute), false).None())
+                        throw c.StandardError.DoesNotContain(new FlagsAttribute(), typeof(T).GetCustomAttributes(false), message);
+
+                    Enum actualEnum = actual as Enum;
+                    Enum expectedFlagEnum = expectedFlag as Enum;
+                    if (!actualEnum.HasFlag(expectedFlagEnum))
+                        throw c.StandardError.DoesNotContain(expectedFlag, Flags<T>(actualEnum), message);
+                });
+        }
+
+        private static IEnumerable<T> Flags<T>(Enum actualEnum) where T : struct
+        {
+            object zeroValue = Enum.Parse(typeof(T), "0");
+            return Enum.GetValues(typeof(T))
+                .Cast<Enum>()
+                .Where(v => !v.Equals(zeroValue) && actualEnum.HasFlag(v))
+                .Cast<T>();
+        }
+    }
+}
---
+++ UnitTests/Assertions/EnumAssertionTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NUnit.Framework;
+using static EasyAssertions.UnitTests.EnumerableArg;
+
+namespace EasyAssertions.UnitTests
+{
+    public class EnumAssertionTests : AssertionTests
+    {
+        [Test]
+        public void ShouldBeValue_SameValue_ReturnsActualValue()
+        {
+            TestEnum actual = TestEnum.One;
+            TestEnum expected = TestEnum.One;
+
+            Actual<TestEnum> result = actual.ShouldBeValue(expected);
+
+            Assert.AreEqual(actual, result.Value);
+        }
+
+        [Test]
+        public void ShouldBeValue_DifferentValue_FailsWithObjectsNotEqualMessage()
+        {
+            TestEnum actual = TestEnum.One;
+            TestEnum expected = TestEnum.Two;
+            Error.NotEqual(expected, actual, "foo").Returns(ExpectedException);
+
+            AssertThrowsExpectedError(() => actual.ShouldBe(expected, "foo"));
+        }
+
+        [Test]
+        public void ShouldHaveFlag_HasFlag_ReturnsActualValue()
+        {
+            FlagsEnum actual = FlagsEnum.One | FlagsEnum.Two;
+            FlagsEnum expectedFlag = FlagsEnum.Two;
+
+            Actual<FlagsEnum> result = actual.ShouldHaveFlag(expectedFlag);
+
+            Assert.AreEqual(actual, result.Value);
+        }
+
+        [Test]
+        public void ShouldHaveFlag_NotAnEnum_FailsWithTypesNotEqualMessage()
+        {
+            AssertFailsWithTypesNotEqualMessage(typeof(Enum), typeof(int), msg => 1.ShouldHaveFlag(1, msg));
+        }
+
+        [Test]
+        public void ShouldHaveFlag_NotAFlagsEnum_FailsWithDoesNotContainFlagsAttributeMessage()
+        {
+            TestEnum actual = TestEnum.One;
+            TestEnum expectedFlag = TestEnum.One;
+            Error.DoesNotContain(Arg.Any<FlagsAttribute>(), Matches<object>(typeof(TestEnum).GetCustomAttributes(false)), "foo").Throws(ExpectedException);
+            
+            AssertThrowsExpectedError(() => actual.ShouldHaveFlag(expectedFlag, "foo"));
+        }
+
+        [Test]
+        public void ShouldHaveFlag_DoesNotHaveFlag_FailsWithDoesNotContainFlagMessage()
+        {
+            FlagsEnum actual = FlagsEnum.Two;
+            FlagsEnum expected = FlagsEnum.One;
+            Error.DoesNotContain(expected, Matches<FlagsEnum>(new[] { FlagsEnum.Two }), "foo").Throws(ExpectedException);
+
+            AssertThrowsExpectedError(() => actual.ShouldHaveFlag(expected, "foo"));
+        }
+
+        [NotFlags]
+        enum TestEnum
+        {
+            One,
+            Two
+        }
+
+        [Flags]
+        enum FlagsEnum
+        {
+            None = 0,
+            One = 1,
+            Two = 2
+        }
+
+        class NotFlagsAttribute : Attribute { }
+    }
+}

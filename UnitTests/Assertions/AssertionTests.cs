50e9419be37995c66e5e479ebae7b94f40345a61
Simon Oxtoby <soxtoby@zaptechnology.com>
2021-09-05 10:26:24 +1000

    Better errors in tests when wrong standard error is created

--- UnitTests/Assertions/AssertionTests.cs
+++ UnitTests/Assertions/AssertionTests.cs
@@ -1,6 +1,10 @@
 using NSubstitute;
 using NUnit.Framework;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 
 namespace EasyAssertions.UnitTests
 {
@@ -12,7 +16,7 @@ namespace EasyAssertions.UnitTests
         [SetUp]
         public void BaseSetUp()
         {
-            Error = Substitute.For<IStandardErrors>();
+            Error = Substitute.ForPartsOf<TestErrors>();
             ExpectedException = new Exception();
             StandardErrors.Override(Error);
             SourceExpressionProvider.ForCurrentThread.Reset();
@@ -53,6 +57,46 @@ namespace EasyAssertions.UnitTests
             var result = Assert.Throws<Exception>(() => assertionCall("foo"));
 
             Assert.AreSame(ExpectedException, result);
+        }
+
+        public class TestErrors : IStandardErrors
+        {
+            readonly Exception unexpectedError = new InvalidOperationException("Unexpected standard error created. Make sure the message was passed through.");
+            public virtual Exception NotEqual(object? expected, object? actual, string? message = null) => unexpectedError;
+            public virtual Exception NotEqual(string expected, string actual, Case caseSensitivity = Case.Sensitive, string? message = null) => unexpectedError;
+            public virtual Exception AreEqual(object? notExpected, object? actual, string? message = null) => unexpectedError;
+            public virtual Exception IsNull(string? message = null) => unexpectedError;
+            public virtual Exception NotSame(object? expected, object? actual, string? message = null) => unexpectedError;
+            public virtual Exception AreSame(object? actual, string? message = null) => unexpectedError;
+            public virtual Exception DoNotMatch<TActual, TExpected>(IEnumerable<TExpected> expected, IEnumerable<TActual> actual, Func<TActual, TExpected, bool> predicate, string? message = null) => unexpectedError;
+            public virtual Exception DoesNotContain(object? expected, IEnumerable actual, string? itemType = null, string? message = null) => unexpectedError;
+            public virtual Exception DoesNotContainItems<TActual, TExpected>(IEnumerable<TExpected> expected, IEnumerable<TActual> actual, Func<TActual, TExpected, bool> predicate, string? message = null) => unexpectedError;
+            public virtual Exception Contains(object? expectedToNotContain, IEnumerable actual, string? itemType = null, string? message = null) => unexpectedError;
+            public virtual Exception Contains(IEnumerable expectedToNotContain, IEnumerable actual, string? message = null) => unexpectedError;
+            public virtual Exception OnlyContains(IEnumerable expected, IEnumerable actual, string? message = null) => unexpectedError;
+            public virtual Exception DoesNotOnlyContain<TActual, TExpected>(IEnumerable<TExpected> expected, IEnumerable<TActual> actual, Func<TActual, TExpected, bool> predicate, string? message = null) => unexpectedError;
+            public virtual Exception ContainsExtraItem<TActual, TExpected>(IEnumerable<TExpected> expectedSuperset, IEnumerable<TActual> actual, Func<TActual, TExpected, bool> predicate, string? message = null) => unexpectedError;
+            public virtual Exception ContainsDuplicate(IEnumerable actual, string? message = null) => unexpectedError;
+            public virtual Exception ItemsNotSame(IEnumerable expected, IEnumerable actual, string? message = null) => unexpectedError;
+            public virtual Exception DoesNotStartWith<TActual, TExpected>(IEnumerable<TExpected> expected, IEnumerable<TActual> actual, Func<TActual, TExpected, bool> predicate, string? message = null) => unexpectedError;
+            public virtual Exception DoesNotEndWith<TActual, TExpected>(IEnumerable<TExpected> expected, IEnumerable<TActual> actual, Func<TActual, TExpected, bool> predicate, string? message = null) => unexpectedError;
+            public virtual Exception TreesDoNotMatch<TActual, TExpected>(IEnumerable<TestNode<TExpected>> expected, IEnumerable<TActual> actual, Func<TActual, IEnumerable<TActual>> getChildren, Func<object?, object?, bool> predicate, string? message = null) => unexpectedError;
+            public virtual Exception NoException(Type expectedExceptionType, LambdaExpression? function = null, string? message = null) => unexpectedError;
+            public virtual Exception WrongException(Type expectedExceptionType, Exception actualException, LambdaExpression? function = null, string? message = null) => unexpectedError;
+            public virtual Exception NotEmpty(IEnumerable actual, string? message = null) => unexpectedError;
+            public virtual Exception NotEmpty(string actual, string? message = null) => unexpectedError;
+            public virtual Exception IsEmpty(string? message = null) => unexpectedError;
+            public virtual Exception LengthMismatch(int expectedLength, IEnumerable actual, string? message = null) => unexpectedError;
+            public virtual Exception DoesNotContain(string expectedSubstring, string actual, string? message = null) => unexpectedError;
+            public virtual Exception Contains(string expectedToNotContain, string actual, string? message = null) => unexpectedError;
+            public virtual Exception DoesNotStartWith(string expectedStart, string actual, string? message = null) => unexpectedError;
+            public virtual Exception DoesNotEndWith(string expectedEnd, string actual, string? message = null) => unexpectedError;
+            public virtual Exception NotGreaterThan(object? expected, object? actual, string? message = null) => unexpectedError;
+            public virtual Exception NotLessThan(object? expected, object? actual, string? message = null) => unexpectedError;
+            public virtual Exception DoesNotMatch(Regex regex, string actual, string? message = null) => unexpectedError;
+            public virtual Exception Matches(Regex regex, string actual, string? message = null) => unexpectedError;
+            public virtual Exception TaskTimedOut(TimeSpan timeout, string? message = null) => unexpectedError;
+            public virtual Exception Matches(IEnumerable notExpected, IEnumerable actual, string? message = null) => unexpectedError;
         }
     }
 }
